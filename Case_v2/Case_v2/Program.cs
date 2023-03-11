bdbaf102403a9bfe66b33002e4babc6ae87757c6
JagerSPB <Jage7@yandex.ru>
2023-03-11 20:12:11 +0300

    case 20 done

--- Case_v2/Case_v2/Program.cs
+++ Case_v2/Case_v2/Program.cs
@@ -112,7 +112,7 @@
             */
             // Case8. Даны два целых числа: D (день) и M (месяц), определяющие правильную дату невисокосного года. 
             //     Вывести значения D и M для даты, предшествующей указанной.
-/*
+            /*
             int m = 3, d = 1;
             switch (m)
             {
@@ -175,8 +175,8 @@
             }
 
             Console.WriteLine($"предшествующий день: {d} \nМесяц: {m}");
-        }
-    */
+            }
+           */
 
 
             // Case16. Дано целое число в диапазоне 20–69, определяющее возраст (в годах). Вывести строку-описание
@@ -233,72 +233,73 @@
             */
             // Case18. Дано целое число в диапазоне 100–999. Вывести строку описание
             // данного числа, например: 256 — «двести пятьдесят шесть», 814 — «восемьсот четырнадцать»
-           /*
-            Console.Write("Введите число: ");
-            int Count = int.Parse(Console.ReadLine());
-            if (Count >= 100 && Count < 1000)
-                switch (Count/100)
-                {
-                    case 1: Console.Write("сто "); break;
-                    case 2: Console.Write("двести "); break;
-                    case 3: Console.Write("триста "); break;
-                    case 4: Console.Write("четыреста "); break;
-                    case 5: Console.Write("пятьсот "); break;
-                    case 6: Console.Write("шестьсот "); break;
-                    case 7: Console.Write("семьсот "); break;
-                    case 8: Console.Write("восемьсот "); break;
-                    case 9: Console.Write("девятьсоть "); break;
-                    
-                }
-            switch (Count / 10 % 10)
-            {
-                case 2: Console.Write("двадцать "); break;
-                case 3: Console.Write("тридцать "); break;
-                case 4: Console.Write("сорок "); break;
-                case 5: Console.Write("пятьдесят "); break;
-                case 6: Console.Write("шестьдесят "); break;
-                case 7: Console.Write("семьдесят "); break;
-                case 8: Console.Write("восемьдесят "); break;
-                case 9: Console.Write("девяносто "); break;
-            }
-           
-            {
-                switch (Count % 100 )
-                {
-                    case 10: Console.Write("десять ");  return;
-                    case 11: Console.Write("одинадцать ");  return;
-                    case 12: Console.Write("двенадцать ");  return;
-                    case 13: Console.Write("тринадцать ");  return;
-                    case 14: Console.Write("четырнадцать ");  return;
-                    case 15: Console.Write("пятнадцать ");  return;
-                    case 16: Console.Write("шестнадцать ");  return;
-                    case 17: Console.Write("семнадцать ");  return;
-                    case 18: Console.Write("восемнадцать "); return;
-                    case 19: Console.Write("дявятнадцать "); return;
-                }
-            }
-            switch (Count % 10)
-            {
-                case 1: Console.Write("один "); return;
-                case 2: Console.Write("два "); break;
-                case 3: Console.Write("три"); return;
-                case 4: Console.Write("четыре "); break;
-                case 5: Console.Write("пять "); break;
-                case 6: Console.Write("шесть "); break;
-                case 7: Console.Write("семь "); break;
-                case 8: Console.Write("восемь "); break;
-                case 9: Console.Write("девять "); break;
-            }
-            */
-           // Case19. В восточном календаре принят 60-летний цикл, состоящий из 12-
-           //     летних подциклов, обозначаемых названиями цвета: зеленый, красный, желтый, белый и черный.
-           //     В каждом подцикле годы носят названия животных: 
-           // крысы, коровы, тигра, зайца, дракона, змеи, лошади, овцы, обезьяны, курицы, собаки и свиньи.
-           //     По номеру года определить его название, если 1984 год — начало цикла: «год зеленой крысы».
+            /*
+             Console.Write("Введите число: ");
+             int Count = int.Parse(Console.ReadLine());
+             if (Count >= 100 && Count < 1000)
+                 switch (Count/100)
+                 {
+                     case 1: Console.Write("сто "); break;
+                     case 2: Console.Write("двести "); break;
+                     case 3: Console.Write("триста "); break;
+                     case 4: Console.Write("четыреста "); break;
+                     case 5: Console.Write("пятьсот "); break;
+                     case 6: Console.Write("шестьсот "); break;
+                     case 7: Console.Write("семьсот "); break;
+                     case 8: Console.Write("восемьсот "); break;
+                     case 9: Console.Write("девятьсоть "); break;
+                     
+                 }
+             switch (Count / 10 % 10)
+             {
+                 case 2: Console.Write("двадцать "); break;
+                 case 3: Console.Write("тридцать "); break;
+                 case 4: Console.Write("сорок "); break;
+                 case 5: Console.Write("пятьдесят "); break;
+                 case 6: Console.Write("шестьдесят "); break;
+                 case 7: Console.Write("семьдесят "); break;
+                 case 8: Console.Write("восемьдесят "); break;
+                 case 9: Console.Write("девяносто "); break;
+             }
             
+             {
+                 switch (Count % 100 )
+                 {
+                     case 10: Console.Write("десять ");  return;
+                     case 11: Console.Write("одинадцать ");  return;
+                     case 12: Console.Write("двенадцать ");  return;
+                     case 13: Console.Write("тринадцать ");  return;
+                     case 14: Console.Write("четырнадцать ");  return;
+                     case 15: Console.Write("пятнадцать ");  return;
+                     case 16: Console.Write("шестнадцать ");  return;
+                     case 17: Console.Write("семнадцать ");  return;
+                     case 18: Console.Write("восемнадцать "); return;
+                     case 19: Console.Write("дявятнадцать "); return;
+                 }
+             }
+             switch (Count % 10)
+             {
+                 case 1: Console.Write("один "); return;
+                 case 2: Console.Write("два "); break;
+                 case 3: Console.Write("три"); return;
+                 case 4: Console.Write("четыре "); break;
+                 case 5: Console.Write("пять "); break;
+                 case 6: Console.Write("шесть "); break;
+                 case 7: Console.Write("семь "); break;
+                 case 8: Console.Write("восемь "); break;
+                 case 9: Console.Write("девять "); break;
+             }
+             */
+            // Case19. В восточном календаре принят 60-летний цикл, состоящий из 12-
+            //     летних подциклов, обозначаемых названиями цвета: зеленый, красный, желтый, белый и черный.
+            //     В каждом подцикле годы носят названия животных: 
+            // крысы, коровы, тигра, зайца, дракона, змеи, лошади, овцы, обезьяны, курицы, собаки и свиньи.
+            //     По номеру года определить его название, если 1984 год — начало цикла: «год зеленой крысы».
+            /*
            Console.Write("Введите число: ");
             int calendar = int.Parse(Console.ReadLine());
             if (calendar >= 1984 && calendar <= 2043)
+            {
                 Console.Write("год ");
                 switch (calendar % 10)
                 {
@@ -309,7 +310,7 @@
                     case 8 or 9: Console.Write("желтый "); break;
                 }
             switch (calendar % 100)
-            {
+                {
                 case 84 or 96 or 08 or 20 or 32: Console.Write("крысы ");  return;
                 case 85 or 97 or 09 or 21 or 33: Console.Write("коровы ");  return;
                 case 86 or 98 or 10 or 22 or 34: Console.Write("тигра ");  return;
@@ -322,8 +323,46 @@
                 case 93 or 05 or 17 or 29 or 41: Console.Write("курицы ");  return;
                 case 94 or 06 or 18 or 30 or 42: Console.Write("собаки ");  return;
                 case 95 or 07 or 19 or 31 or 43: Console.Write("свиньи ");  return;
+                }
             }
-            
+            else 
+                Console.WriteLine("Error");
+            */
+            // Case20. Даны два целых числа: D (день) и M (месяц), определяющие правильную дату.Вывести знак Зодиака, 
+            //     соответствующий этой дате: «Водолей» (20.1–18.2), «Рыбы» (19.2–20.3), «Овен» (21.3–19.4),
+            //     «Телец» (20.4– 20.5), «Близнецы» (21.5–21.6), «Рак» (22.6–22.7), «Лев» (23.7–22.8), «Дева» (23.8–22.9),
+            //     «Весы» (23.9–22.10), «Скорпион» (23.10–22.11), «Стрелец» (23.11–21.12), «Козерог» (22.12–19.1).
+            Console.Write("Введите день: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.Write("Введите месяц: ");
+            int month = int.Parse(Console.ReadLine());
+            switch (month)
+            {
+                case 1: if (day > 19) Console.WriteLine("Водолей");
+                    else Console.WriteLine("козерог"); return;
+                case 2: if (day > 18) Console.WriteLine("Рыбы");
+                    else Console.WriteLine("Водолей"); return;
+                case 3: if (day > 20) Console.WriteLine("Овен");
+                    else Console.WriteLine("Рыбы"); return;
+                case 4: if (day > 19) Console.WriteLine("Телец");
+                    else Console.WriteLine("Овен"); return;
+                case 5: if (day > 20) Console.WriteLine("Близнецы");
+                    else Console.WriteLine("Телец"); return;
+                case 6: if (day > 21) Console.WriteLine("Рак");
+                    else Console.WriteLine("Близнецы"); return;
+                case 7: if (day > 22) Console.WriteLine("Лев");
+                    else Console.WriteLine("Рак"); return;
+                case 8: if (day > 22) Console.WriteLine("Дева");
+                    else Console.WriteLine("Лев"); return;
+                case 9: if (day > 22) Console.WriteLine("Весы");
+                    else Console.WriteLine("Дева"); return;
+                case 10: if (day > 22) Console.WriteLine("Скорпион");
+                    else Console.WriteLine("Весы"); return;
+                case 11: if (day > 22) Console.WriteLine("Стрелец");
+                    else Console.WriteLine("Скорпион"); return;
+                case 12: if (day > 21) Console.WriteLine("Козерог");
+                    else Console.WriteLine("Стрелец"); return;
+            }
         }
     }
 }
