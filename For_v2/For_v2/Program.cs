68da51b55d6c76446557614e35a6f5687aa0996f
JagerSPB <Jage7@yandex.ru>
2023-03-17 20:05:11 +0300

    37 done

---
+++ For_v2/For_v2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace For_v2
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            //For1. Даны целые числа K и N (N > 0). Вывести N раз число K.
+            /*
+            int k = 3, n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"[{i}] -> {k}");
+            }
+            */
+            // For2. Даны два целых числа A и B (A < B). Вывести в порядке возрастания все целые числа, 
+            //     расположенные между A и B (включая сами числа A и B), а также количество N этих чисел.
+            /*
+            int a = 4, b = 8, count = 0;
+            for (int i = a; i <= b; i++)
+            {
+                Console.WriteLine($"[{i}]");
+                count++;
+            }
+            Console.WriteLine($"Всего: {count} ");
+            */
+        }
+    }
+}
