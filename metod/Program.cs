3ded6c3b6850bbe5cc654532e2d3b2cde6d095a8
JagerSPB <Jage7@yandex.ru>
2023-04-11 16:26:46 +0300

    13 done

--- metod/Program.cs
+++ metod/Program.cs
@@ -225,30 +225,29 @@ namespace Method
             Console.WriteLine(result2);
             //*/
             //Proc8
-            Console.Write("Введите число: ");
-            int d = int.Parse(Console.ReadLine());
-            int k = 8;
-            int result = Proc8AddRightDigit(d, k);
-            Console.WriteLine(result);
+            // Console.Write("Введите число: ");
+            // int x = int.Parse(Console.ReadLine());
+            double a = 10, b = 20, c = 3;
+            Proc13SortDec3(ref a, ref b, ref c);
+            Console.WriteLine($"{a}  {b}  {c}");
         }
 
-        /*
-       /// <summary>
-       /// Proc2.
-       /// </summary>
-       /// <param name="a"></param>
-       /// <param name="b"></param>
-       /// <param name="c"></param>
-       /// <param name="d"></param>
-     
-       static void Proc2PowerA234(double a, out double b, out double c, out double d)
-       {
-           b = Math.Pow(a, 2);
-           c = Math.Pow(a, 3);
-           d = Math.Pow(a, 4);
-       }
-       */
-        /*
+
+        /// <summary>
+        /// Proc2.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        static void Proc2PowerA234(double a, out double b, out double c, out double d)
+        {
+            b = Math.Pow(a, 2);
+            c = Math.Pow(a, 3);
+            d = Math.Pow(a, 4);
+        }
+
+
         /// <summary>
         /// Proc3.Описать процедуру Mean(X, Y, AMean, GMean), вычисляющую среднее арифметическое AMean = (X+Y)/2 
         /// и среднее геометрическое GMean = √X·Y двух положительных чисел X и Y (X и Y — входные, 
@@ -259,13 +258,13 @@ namespace Method
         /// <param name="value2"></param>
         /// <param name="aMean"></param>
         /// <param name="gMean"></param>
-         static void Proc3Mean(double value1, double value2, out double aMean, out double gMean)
-         {
+        static void Proc3Mean(double value1, double value2, out double aMean, out double gMean)
+        {
             aMean = (value1 + value2) / 2;
             gMean = Math.Sqrt(value1 * value2);
-         }
-        */
-        /*
+        }
+
+
         /// <summary>
         /// Proc4. Описать процедуру TrianglePS(a, P, S), вычисляющую по стороне a равностороннего треугольника его 
         /// периметр P = 3·a и площадь S = a2·√3/4 (a — входной, P и S — выходные параметры; все
@@ -278,8 +277,8 @@ namespace Method
             p = a * 3;
             s = (a * a * Math.Sqrt(3)) / 4;
         }
-        */
-        /*
+
+
         /// <summary>
         /// Proc6.Описать процедуру DigitCountSum(K, C, S), находящую количество C цифр целого положительного числа K, 
         /// а также их сумму S (K — входной, C и S — выходные параметры целого типа). С помощью этой процедуры найти 
@@ -300,8 +299,7 @@ namespace Method
                 k /= 10;
             }
         }
-        */
-        /*
+
         /// <summary>
         ///  Proc7. Описать процедуру InvertDigits(K), меняющую порядок следования цифр целого положительного числа K на 
         /// обратный (K — параметр целого типа, являющийся одновременно входным и выходным).С помощью этой процедуры
@@ -322,8 +320,7 @@ namespace Method
 
             return invers;
         }
-        */
-        // /*
+
         /// <summary>
         /// Proc8◦. Описать процедуру AddRightDigit(D, K), добавляющую к целому по- ложительному числу K справа цифру D
         /// (D — входной параметр целого типа, лежащий в диапазоне 0–9, K — параметр целого типа, являющийся
@@ -333,11 +330,107 @@ namespace Method
         /// <param name="d"></param>
         /// <param name="k"></param>
         /// <returns></returns>
-        static int Proc8AddRightDigit(int d, int k)
+        static int Proc8AddRightDigit(int d, int k) => k * 10 + d;
+
+        static int Proc8AddRightDigitV2(int d, int k) => Convert.ToInt32(k + d.ToString());
+
+        /// <summary>
+        /// Proc9. Описать процедуру AddLeftDigit(D, K), добавляющую к целому по- ложительному числу K слева цифру D
+        /// (D — входной параметр целого типа, лежащий в диапазоне 1–9, K — параметр целого типа,
+        /// являющийся одновременно входным и выходным).
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        static int Proc9AddRightDigit(int d, int k) => Convert.ToInt32(d + k.ToString());
+
+        /// <summary>
+        /// Proc10. Описать процедуру Swap(X, Y), меняющую содержимое перемен- ных X и Y
+        /// (X и Y — вещественные параметры, являющиеся одновременно входными и выходными).
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        static void Proc10Swap(ref double x, ref double y)
         {
-            int newNum = d * 10 + k;
-            return newNum;
+            double temp = x;
+            x = y;
+            y = temp;
         }
-        // */
+
+        /// <summary>
+        /// Proc11. Описать процедуру Minmax(X, Y), записывающую в переменную X минимальное из значений X и Y, а в
+        /// переменную Y — максимальное из этих значений (X и Y — вещественные параметры, являющиеся одновременно
+        /// входными и выходными).
+        /// </summary>
+        /// <param name="???"></param>
+        /// <param name="???"></param>
+        static void Proc11Minmax(ref double x, ref double y)
+        {
+            double min = x;
+            if (y < min)
+            {
+                x = y;
+                y = min;
+            }
+        }
+
+        /// <summary>
+        /// Proc12. Описать процедуру SortInc3(A, B, C), меняющую содержимое пере- менных A, B, C таким образом, чтобы
+        /// ихзначения оказались упорядоченными по возрастанию (A, B, C — вещественные параметры, являющиеся
+        /// одновременно входными и выходными).
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        static void Proc12SortInc3(ref double a, ref double b, ref double c)
+        {
+            if (b < a)
+            {
+                double tempNum = a;
+                a = b;
+                b = tempNum;
+            }
+
+            if (c < b)
+            {
+                double tempNum = b;
+                b = c;
+                c = tempNum;
+
+                if (b < a)
+                {
+                    tempNum = a;
+                    a = b;
+                    b = tempNum;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Proc13. Описать процедуру SortDec3(A, B, C), меняющую содержимое переменных A, B, C таким образом,
+        /// чтобы их значения оказались упорядоченными по убыванию
+        /// (A, B, C — вещественные параметры, являющиеся одновременно входными и выходными).
+        /// </summary>
+        static void Proc13SortDec3(ref double a, ref double b, ref double c)
+        {
+            if (a < b)
+            {
+                double tempNum = a;
+                a = b;
+                b = tempNum;
+            }
+            if (a < c)
+            {
+                double tempNum = a;
+                a = c;
+                c = tempNum;
+            }
+            if (b < c)
+            {
+                double tempNum = b;
+                b = c;
+                c = tempNum;
+            }
+        }
     }
 }
