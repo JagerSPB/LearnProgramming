9804293e192cc0c36028e3b97bb860c2b604cfa1
JagerSPB <Jage7@yandex.ru>
2023-04-16 16:36:30 +0300

    Array restart

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,57 +1,66 @@
-﻿using System;
-
-namespace Array
+﻿namespace Array
 {
     internal static class Program
     {
+        private static Random rnd = new Random(DateTime.Now.Millisecond);
+
+
         static void Main()
         {
-            //int[] myArray = { 1, 4, 6, 2, 5, 2, 8, 9, 2, 1 };
-
-            int[] myArray = new int[30];
-            FullArray(myArray, 1, 999);
-            Insert(ref myArray, -5000, 5);
-            PrintArray(myArray);
-        }
-
-        static void FullArray(int[] array, int startNum, int finishNum)
-        {
-            Random random = new Random();
-            for (int i = 0; i < array.Length; i++)
-                array[i] = random.Next(startNum, finishNum);
-        }
-
-        static void PrintArray(int[] array)
-        {
-            foreach (var element in array)
-                Console.Write(element + " ");
-
-
+            //=====================================================
+            // Console.Write("Введите размер массива: ");
+            // int n = Int32.Parse(Console.ReadLine());
+            // int[] array = new int[n];
+            // for (int i = 0; i < array.Length;)
+            //     array[i++] = rnd.Next(100);
             // for (int i = 0; i < array.Length; i++)
-            //     Console.Write(array[i] + " ");
-        }
-
-
-        static void AddFirst(ref int[] array, int value)
-        {
-            Insert(ref array, value, 0);
-        }
-
-        static void AddLast(ref int[] array, int value)
-        {
-            Insert(ref array, value, array.Length);
-        }
-
-
-        static void Insert(ref int[] array, int value, int index)
-        {
-            int[] newArray = new int[array.Length + 1];
-            newArray[index] = value;
-            for (int i = 0; i < index; i++)
-                newArray[i] = array[i];
-            for (int i = index; i < array.Length; i++)
-                newArray[i + 1] = array[i];
-            array = newArray;
+            //     Console.WriteLine($"[{i}] = {array[i]}");
+            //=====================================================
+            // Array1. Дано целое число N (>0). Сформировать и вывести целочисленный массив размера N, содержащий N
+            // первых положительных нечетных чисел: 1, 3, 5, . . . .
+            /*
+            Console.Write("Введите размер массива: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int oddNum = 1;
+            for (int i = 0; i < array.Length; i++, oddNum += 2)
+                array[i] = oddNum;
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"[{i}] = {array[i]}");
+            */
+            // Array2. Дано целое число N (>0). Сформировать и вывести целочисленный массив размера N, содержащий степени
+            //     двойки от первой до N-й: 2, 4, 8, 16, . . . .
+            /*
+            Console.Write("Введите размер массива: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int pow2 = 2;
+            for (int i = 0; i < array.Length; i++, pow2 *= 2)
+                array[i] = pow2;
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"[{i}] = {array[i]}");
+            */
+            // Array3. Дано целое число N (> 1), а также первый член A и разность D арифметической прогрессии.Сформировать 
+            //     и вывести массив размера N, содержащий N первых членов данной прогрессии: A, A+D, A+2·D, A+3·D,
+            /*
+            int n = 10, a = 2, d = 3;
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = a + i * d;
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"[{i}] = {array[i]}");
+            */
+            // Array4. Дано целое число N (>1), а также первый член A и знаменатель D геометрической прогрессии. 
+            // Сформировать и вывести массив размера N, содержащий N первых членов данной прогрессии: A, A·D, A·D2, A·D3, . 
+            /*
+            int n = 10;
+            double a = 2, d = 3;
+            double[] array = new double[n];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = a * Math.Pow(d, i);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"[{i}] = {array[i]}");
+            */
         }
     }
 }
