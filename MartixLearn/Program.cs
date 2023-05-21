1e691e4c3da0357439b7ec9d71c85b10350feed7
JagerSPB <Jage7@yandex.ru>
2023-05-21 12:27:27 +0300

    Matrix

---
+++ MartixLearn/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Matrix
+{
+    internal static class Program
+    {
+        private static Random rnd = new Random(DateTime.Now.Millisecond);
+
+        public static void ShowMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("[ ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine("]");
+            }
+        }
+
+        /// <summary>
+        /// Matrix1. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M × N, у которой
+        /// все элементы I-й строки имеют значение 10·I (I = 1,...,M).
+        /// </summary>
+        public static void Matrix1()
+        {
+            int rows = 4, columns = 5;
+            int[,] matrix = new int[rows, columns];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = 10 * (i + 1);
+                }
+            }
+
+            ShowMatrix(matrix);
+        }
+
+        /// <summary>
+        /// Matrix2. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M × N, у которой
+        /// все элементы J-го столбца имеют значение 5·J (J = 1, . . ., N).
+        /// </summary>
+        public static void Matrix2()
+        {
+            int[,] matrix = new int[4, 4];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = 5 * (j + 1);
+                }
+            }
+            ShowMatrix(matrix);
+        }
+
+        /// <summary>
+        /// Matrix3. Даны целые положительные числа M, N и набор из M чисел. Сформировать матрицу размера M × N, у
+        /// которой в каждом столбце содержатся все числа из исходного набора (в том же порядке).
+        /// </summary>
+        public static void Matrix3()
+        {
+            int[] numbers = { 2, 3, 4, 5 };
+            
+            int[,] matrix = new int[4, 6];
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix[i, j] = numbers[i];
+                }
+                
+            }
+            ShowMatrix(matrix);
+        }
+
+        static void Main()
+        {
+            Matrix3();
+        }
+    }
+}
