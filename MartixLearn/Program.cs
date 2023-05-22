a0e808ae1f7a99d515f5f458717e30fbd846e50e
JagerSPB <Jage7@yandex.ru>
2023-05-22 10:25:06 +0300

    task 6

--- MartixLearn/Program.cs
+++ MartixLearn/Program.cs
@@ -43,14 +43,15 @@
         /// </summary>
         public static void Matrix2()
         {
-            int[,] matrix = new int[4, 4];
+            int[,] matrix = new int[4, 5];
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    matrix[i, j] = 5 * (j + 1);
+                    matrix[i, j] = 5 * (j+1) ;
                 }
             }
+
             ShowMatrix(matrix);
         }
 
@@ -60,23 +61,93 @@
         /// </summary>
         public static void Matrix3()
         {
-            int[] numbers = { 2, 3, 4, 5 };
-            
-            int[,] matrix = new int[4, 6];
-            for (int j = 0; j < matrix.GetLength(1); j++)
+            int[] array = { 2, 3, 4, 5 };
+            int[,] matrix = new int[6, array.Length];
+            for (int i = 0; i < matrix.GetLength(1); i++)
             {
-                for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(0); j++)
                 {
-                    matrix[i, j] = numbers[i];
+                    matrix[j, i] = array[i];
                 }
-                
             }
+
+            ShowMatrix(matrix);
+        }
+
+        /// <summary>
+        /// Matrix4. Даны целые положительные числа M, N и набор из N чисел. Сформировать матрицу размера M × N,
+        /// у которой в каждой строке содержатся все числа из исходного набора (в том же порядке).
+        /// </summary>
+        public static void Matrix4()
+        {
+            int[] array = { 2, 3, 4, 5 };
+            int[,] matrix = new int[6,array.Length];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    matrix[j, i] = array[i];
+                }
+            }
+
+            ShowMatrix(matrix);
+        }
+        /// <summary>
+        /// Matrix5. Даны целые положительные числа M, N, число D и набор из M чисел. Сформировать матрицу размера
+        /// M × N, у которой первый столбец совпадает с исходным набором чисел, а элементы каждого следующего
+        /// столбца равны сумме соответствующего элемента предыдущего столбца и числа D (в результате каждая
+        /// строка матрицы будет содержать элементы арифметической прогрессии).
+        /// </summary>
+        public static void Matrix5()
+        {
+            int[] array = { 2, 3, 4, 5 };
+            int d = 5;
+            int[,] matrix = new int[4, 4 ];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = array[i];
+            }
+            
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 1; j < matrix.GetLength(0); j++)
+                {
+                    matrix[i, j] =  matrix[i, j-1]*2 + d;
+                }
+            }
+
+            ShowMatrix(matrix);
+        }
+        /// <summary>
+        /// Matrix6. Даны целые положительные числа M, N, число Q и набор из N чисел. Сформировать матрицу размера
+        /// M × N, у которой первая строка совпадает с исходным набором чисел, а элементы каждой следующей строки
+        /// равны соответствующему элементу предыдущей строки, умноженному на Q (в результате каждый столбец матрицы
+        /// будет содержать элементы геометрической прогрессии).
+        /// </summary>
+        public static void Matrix6()
+        {
+            int[] array = { 2, 3, 4, 5 };
+            int q = 10;
+            int[,] matrix = new int[array.Length, 4];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[0, i] = array[i];
+            }
+            
+            for (int i = 1; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    matrix[i, j] =  matrix[i-1, j]*q;
+                }
+            }
+
             ShowMatrix(matrix);
         }
 
         static void Main()
         {
-            Matrix3();
+            Matrix6();
         }
     }
 }
