721a5228933ea0e75ddf776a23deb1b6837f7d81
JagerSPB <Jage7@yandex.ru>
2023-10-19 12:16:34 +0300

    Calc v2

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,18 +16,56 @@
         int action = int.Parse(Console.ReadLine());
         switch (action)
         {
-            case 1: result = num1 + num2; break;
-            case 2: result = num1 - num2; break;
-            case 3: result = num1 * num2; break;
-            case 4: result = num1 / num2; break;
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                result = num1 / num2;
+                break;
         }
 
         Console.WriteLine($"результат вычисления: {result}");
     }
-    
+
+    public static void CalculatorV2()
+    {
+        Console.WriteLine("Введите первое число: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите первое число: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите ифру \"1\" для суммирования");
+        Console.WriteLine("Введите ифру \"2\" для вычитания");
+        Console.WriteLine("Введите ифру \"3\" для умножения");
+        Console.WriteLine("Введите ифру \"4\" для деления");
+        int action = int.Parse(Console.ReadLine());
+        switch (action)
+        {
+            case 1:
+                Console.WriteLine($"результат сложения {num1} и {num2} будет: {num1 + num2}");
+                break;
+            case 2:
+                Console.WriteLine($"результат вычитания из {num1} цифры {num2} будет: {num1 - num2}");
+                break;
+            case 3:
+                Console.WriteLine($"результат множения {num1} на {num2} будет: {num1 * num2}");
+                break;
+            case 4:
+                Console.WriteLine($"результат деления {num1} на {num2} будет: {num1 / num2}");
+                break;
+        }
+    }
+
     public static void Main()
     {
-        Calculator();
+        CalculatorV2();
         Console.ReadLine();
     }
 }
