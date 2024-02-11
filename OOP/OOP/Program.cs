4654e91a736097ccd3bcdf032399976a64ae88e8
JagerSPB <Jage7@yandex.ru>
2024-02-11 16:55:27 +0300

    OOP begin

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Car myCar1 = new Car();
+        Console.WriteLine(myCar1.color);
+        myCar1.ChangeColor("Red");
+        Console.WriteLine(myCar1.color);
+        Car myCar2 = new Car();
+        myCar2.speed = 100;
+        myCar2.color = "Black";
+        myCar2.isCrashed = true;
+        myCar2.model = "BMW";
+        Console.WriteLine($"Скорость:{myCar2.speed}, Color:{myCar2.color}, была в аварии {myCar2.isCrashed}");
+        myCar1.CurrentSpeed();
+        myCar2.CurrentSpeed();
+        
+        Console.ReadLine();
+    }
+}
+
+
+class Car
+{
+    public int speed;
+    public string color = "Green";
+    public string model;
+    public int age;
+    public bool isCrashed;
+
+    public void CurrentSpeed()
+    {
+        Console.WriteLine($"Текущая скорость: {speed}");
+    }
+
+    public void ChangeColor(string col)
+    {
+        color = col;
+    }
+}
---
+++ Test Learning/Test Learning/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+class Programm
+{
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    };
+
+
+    static void Test1()
+    {
+        DaysOfWeek day = DaysOfWeek.Monday;
+        Console.WriteLine((int)day + 1);
+    }
+
+    static void Test2()
+    {
+        int sum = 1, max = 6, i = 2;
+        while (i <= max)
+        {
+            sum *= i;
+            i++;
+        }
+        Console.WriteLine(sum);
+    }
+
+    static void Main()
+    {
+        Test2();
+        Console.ReadLine();
+    }
+}
